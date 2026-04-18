7a279f797c77dac604adab3237ecaa3b6e04c28e
Krizia Thais Rujel Acosta <kriziathais2007@gmail.com>
2026-04-18 00:17:53 -0500

    ud

---
+++ backend/src/test/java/com/senati/RestauranteMili/RestauranteMiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senati.RestauranteMili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteMiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
